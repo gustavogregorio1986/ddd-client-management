ef60969564cf2ebcd7a2d41aff470e3156514100
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-09 14:10:01 -0300

    estudando n9ode js com persistencia no banco de dados mysql, para evoluir mais em node js e ter curriculo e ter algum projeto em node com orm e migration para apresentar

---
+++ models/Client.js
@@ -0,0 +1,26 @@
+// src/infrastructure/models/Client.js
+module.exports = (sequelize, DataTypes) => {
+    const Client = sequelize.define('Client', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    });
+  
+    return Client;
+  };
+  
